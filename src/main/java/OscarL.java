44ab217c1ec82a8495871b734b004fc4a99d6631
Liu-Jihan <142770350+LiuJihan@users.noreply.github.com>
2025-01-23 15:17:17 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/OscarL.java
@@ -0,0 +1,8 @@
+public class OscarL {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------\n"+"Hello! I'm OscarL\n" +
+                "What can I do for you?\n"+"-------------------------------------------------------\n");
+        System.out.println("-------------------------------------------------------\n"+
+                "Bye. Hope to see you again soon!\n" + "-------------------------------------------------------\n");
+    }
+}
