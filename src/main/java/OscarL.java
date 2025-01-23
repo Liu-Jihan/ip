1d8173ffd1b2977bb55f7e8b5d00ad51a7f93292
Liu-Jihan <142770350+LiuJihan@users.noreply.github.com>
2025-01-23 15:49:07 +0800

    add an arrayList to memorize (Level 2. Add, List)

--- src/main/java/OscarL.java
+++ src/main/java/OscarL.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class OscarL {
     public static void main(String[] args) {
         System.out.println("-------------------------------------------------------\n" + "Hello! I'm OscarL\n" +
                 "What can I do for you?\n" + "-------------------------------------------------------\n");
 
+        ArrayList<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+
         String theInput;
 
 
@@ -18,10 +21,17 @@ public class OscarL {
                 System.out.println("____________________________________________________________");
                 break;
             }
-
-            // Echo the user's input
-            System.out.println(theInput);
-            System.out.println("____________________________________________________________");
+            else if (theInput.equals("list")) { // Display all tasks
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks.add(theInput);
+                System.out.println("added: " + theInput);
+                System.out.println("____________________________________________________________");
+            }
         }
 
         scanner.close();
