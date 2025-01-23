97561a3b041237536706ff493b9778010dc2d885
Liu-Jihan <142770350+LiuJihan@users.noreply.github.com>
2025-01-23 15:35:00 +0800

    Level 2. Add, List

--- src/main/java/OscarL.java
+++ src/main/java/OscarL.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class OscarL {
     public static void main(String[] args) {
-        System.out.println("-------------------------------------------------------\n"+"Hello! I'm OscarL\n" +
-                "What can I do for you?\n"+"-------------------------------------------------------\n");
-        System.out.println("-------------------------------------------------------\n"+
-                "Bye. Hope to see you again soon!\n" + "-------------------------------------------------------\n");
+        System.out.println("-------------------------------------------------------\n" + "Hello! I'm OscarL\n" +
+                "What can I do for you?\n" + "-------------------------------------------------------\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String theInput;
+
+
+
+        while (true) {
+            theInput = scanner.nextLine();
+
+            if (theInput.equals("bye")) { // Exit condition
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            // Echo the user's input
+            System.out.println(theInput);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
+
     }
 }
