0dd7286871cf965ffc8a12d6e506ec3aa40d13fe
Liu-Jihan <142770350+LiuJihan@users.noreply.github.com>
2025-02-05 02:57:11 +0800

    Add Ui to store welcome message Add Command class for all the store execution OscarL:Logic has been simplified

---
+++ src/main/java/Ui.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        System.out.println("-------------------------------------------------------\n" +
+                "Hello! I'm OscarL\nWhat can I do for you?\n" +
+                "-------------------------------------------------------");
+    }
+
+    public void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public String readCommand() {
+        return scanner.nextLine().trim();
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void showError(String errorMessage) {
+        System.out.println("OOPS!!! " + errorMessage);
+    }
+}
